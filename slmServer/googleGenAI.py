7434fc655aa410eeb64bb4bdb5f3b4f3109f9a6a
avinashsinghavin <avinashsinghavin@gmail>
2024-04-08 17:33:12 +0530

    Final Changes

--- slmServer/googleGenAI.py
+++ slmServer/googleGenAI.py
@@ -1,13 +1,14 @@
 import google.generativeai as palm
 
-palm.configure(api_key = "AIzaSyBh2vOXx9sOrRVVGF0pSq5OACBA87V7cps")
+palm.configure(api_key="AIzaSyBh2vOXx9sOrRVVGF0pSq5OACBA87V7cps")
 
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
 
-model = models[0].name
+def generateText(userInput):
+    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+    model = models[0].name
 
-userInput = input("enter: ")
-completions = palm.generate_text(model = model, prompt = userInput, temperature = 0.5, max_output_tokens = 200);
+    inputPrompt = "can you please response as a chat for for question asked " + userInput
+    completions = palm.generate_text(model=model, prompt=inputPrompt, temperature=0.7, max_output_tokens=200)
 
-ans = completions.result
-print(ans)
+    ans = completions.result
+    return ans
--- slmServer/server.py
+++ slmServer/server.py
@@ -2,13 +2,14 @@ from concurrent import futures
 import grpc
 import chatbot_pb2
 import chatbot_pb2_grpc
+import googleGenAI
 
 class ChatbotServiceServicer(chatbot_pb2_grpc.ChatbotServiceServicer):
     def SayHello(self, request, context):
         return chatbot_pb2.HelloReply(message='Ping! ' + request.name)
 
     def GetBotData(self, request, context):
-        return chatbot_pb2.ChatBotResponse(response='Hello, ' + request.request + '!')
+        return chatbot_pb2.ChatBotResponse(response=googleGenAI.generateText(request.request))
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
